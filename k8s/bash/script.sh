312b26efa1d68c430d2ec9503efbaedc748ff1e1
Damola12345 <ajiboladamola789@gmail.com>
2022-04-04 17:11:57 +0100

    script

--- k8s/bash/script.sh
+++ k8s/bash/script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cd /home/dammytestcloud/Downloads/ABS-GUIDE/k8s
+
 NAMESPACE='abs-guide'
 
-kubectl create namespace $NAMESPACE 
-kubectl apply -n $NAMESPACE -f deployment.yaml
-kubectl apply -n $NAMESPACE -f service.yaml
+kubectl create namespace $NAMESPACE
+kubectl apply -n $NAMESPACE -f deployment.yml
+kubectl apply -n $NAMESPACE -f service.yml
 kubectl get all -n $NAMESPACE
+minikube service -n $NAMESPACE abs-guide --url
--- script/docker_deploy.sh
+++ script/docker_deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+cd /home/dammytestcloud/Downloads/ABS-GUIDE
+echo "build the docker image"
+
 docker build -t abs-guide .
+
+echo "Deploying the updated container"
+
 docker run -it -p 8081:80 abs-guide
